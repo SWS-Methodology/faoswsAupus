a6b19dbcb9d13618ef6560980973784e5c31c8ab
rockclimber112358 <rockclimber112358@gmail.com>
2015-07-15 11:43:29 +0200

    Added documentation to allocateDelta.R

--- R/allocateDelta.R
+++ R/allocateDelta.R
@@ -1,14 +1,33 @@
 ##' Allocate Delta via Maximum Likelihood
 ##' 
+##' This function assumes that a sum of several elements has been updated and 
+##' that the adjustment must be split up into each of the individual elements in
+##' the sum.  Thus, we start with an equation like:
+##' 
+##' X_1 + X_2 + ... + X_N = Y
+##' 
+##' Y becomes Y + Delta, and we must determine a new equation such that
+##' 
+##' (X_1 + Delta_1) + (X_2 + Delta_2) + ... + (X_N + Delta_N) = Y + Delta
+##' 
+##' The individual delta values should be determined via maximum likelihood, as 
+##' we have a distribution for each of these elements.  We can simplify the 
+##' problem a bit by subtracting Y from both sides:
+##' 
+##' Delta_1 + Delta_2 + ... + Delta_N = Delta
+##' 
 ##' @param param1 A vector of the first parameter for each of the elements.  For
 ##'   a normal distribution, this is the mean.
 ##' @param param2 A vector of the second parameter for each of the elements. For
 ##'   a normal distribution, this is the standard deviation.
 ##' @param dist A vector of the name of the distribution for each distribution. 
-##'   Currently, only "Normal" is allowed.
-##' @param sign A vector of the sign of each element.  These values should all
-##'   be +1 or -1.
-##'   
+##'   Currently, only "Normal" is implemented.
+##' @param sign A vector of the sign of each element.  These values should all 
+##'   be +1 or -1, and they indicate if Delta_1, Delta_2, ... should be
+##'   pre-multiplied by a negative or not.  Usually, these will all be +1.
+##' 
+##' @return A vector of the final values (Delta_1, Delta_2, ..., Delta_N)
+##' 
 
 allocateDelta = function(param1, param2, dist = rep("Normal", length(param1)), sign){
     ## Input Checks
